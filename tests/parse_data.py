1333fe879aeddedeb33f92c50531fdb7f74825f8
jf <jokester425@gmail.com>
2022-12-07 22:47:18 -0800

    non-byte-aligned bit fields are passings tests now

--- tests/parse_data.py
+++ tests/parse_data.py
@@ -43,8 +43,6 @@ def get_uint_value(value: int, bit_count: int, endian: Literal["little", "big"])
 
 def get_bitarray(value: int, bit_count: int, endian: Literal["little", "big"]) -> bitarray:
     byte_val = get_bytes(value=value, bit_count=bit_count, endian=endian)
-    # _byte_val = bytearray(byte_val)
-    # _byte_val.reverse()
     bits = bitarray(endian="big")
     bits.frombytes(byte_val)
     if bit_count == 0:
@@ -53,14 +51,6 @@ def get_bitarray(value: int, bit_count: int, endian: Literal["little", "big"]) -
         b = bits
     else:
         b = bits[-bit_count:]
-        # if bit_count <= 8:
-        # elif bit_count <= 16:
-        #     if endian == "big":
-        #         b = bits[-bit_count:]
-        #     else:
-        #         b = bits[:8] + bits[-(bit_count - 8) :]
-        # else:
-        #     b = bits
     return b
 
 
@@ -83,11 +73,66 @@ def get_bytes(value: int, bit_count: int, endian: Literal["little", "big"]) -> b
                 b = b
             else:
                 b = b[:1]
+    elif value < 0x100000000 and bit_count <= 32:
+        if endian == "big":
+            b = struct.pack(">I", value)
+            if bit_count > 24:
+                b = b
+            elif bit_count > 16:
+                b = b[1:]
+            elif bit_count > 8:
+                b = b[2:]
+            else:
+                b = b[3:]
+        else:
+            b = struct.pack("<I", value)
+            if bit_count > 24:
+                b = b
+            elif bit_count > 16:
+                b = b[:3]
+            elif bit_count > 8:
+                b = b[:2]
+            else:
+                b = b[:1]
+    elif value < 0x10000000000000000 and bit_count <= 64:
+        if endian == "big":
+            b = struct.pack(">Q", value)
+            if bit_count > 56:
+                b = b
+            elif bit_count > 48:
+                b = b[1:]
+            elif bit_count > 40:
+                b = b[2:]
+            elif bit_count > 32:
+                b = b[3:]
+            elif bit_count > 24:
+                b = b[4:]
+            elif bit_count > 16:
+                b = b[5:]
+            elif bit_count > 8:
+                b = b[6:]
+            else:
+                b = b[7:]
+        else:
+            b = struct.pack("<Q", value)
+            if bit_count > 56:
+                b = b
+            elif bit_count > 48:
+                b = b[:7]
+            elif bit_count > 40:
+                b = b[:6]
+            elif bit_count > 32:
+                b = b[:5]
+            elif bit_count > 24:
+                b = b[:4]
+            elif bit_count > 16:
+                b = b[:3]
+            elif bit_count > 8:
+                b = b[:2]
+            else:
+                b = b[:1]
     else:
         b = b""
-    # b = bytearray(b)
-    # b.reverse()
-    # b = bytes(b)
     if bit_count > 0:
         return b
     else:
@@ -200,11 +245,14 @@ TEST_VALUES_08_BIT_UINT_LE_VARIABLE = [
 ]
 
 
-_TEST_VALUES_16_BIT = list(
-    set(
-        list([(2**i) for i in range(0, 16, 4) if (2**i) < 0x10000])
-        + list([(2**i) - 1 for i in range(0, 16 + 4, 4)])
+_TEST_VALUES_16_BIT = (
+    list(
+        set(
+            list([(2**i) for i in range(0, 16, 4) if (2**i) < 0x10000])
+            + list([(2**i) - 1 for i in range(0, 16 + 4, 4)])
+        )
     )
+    + _TEST_VALUES_08_BIT
 )
 _TEST_VALUES_16_BIT.sort()
 TEST_VALUES_16_BIT = [(value, 16) for value in _TEST_VALUES_16_BIT]
@@ -307,8 +355,90 @@ TEST_VALUES_16_BIT_UINT_LE_VARIABLE = [
     for value, bit_count in _TEST_VALUES_16_BIT_UINT_LE
 ]
 
-TEST_VALUES_N_BIT_COUNTS_BE = list(set(_TEST_VALUES_08_BIT_UINT_BE + _TEST_VALUES_16_BIT_UINT_BE))
-TEST_VALUES_N_BIT_COUNTS_LE = list(set(_TEST_VALUES_08_BIT_UINT_LE + _TEST_VALUES_16_BIT_UINT_LE))
+_TEST_VALUES_32_BIT = (
+    list(
+        set(
+            list([(2**i) for i in range(0, 32, 8) if (2**i) < 0x100000000])
+            + list([(2**i) - 1 for i in range(0, 32 + 8, 8)])
+        )
+    )
+    + _TEST_VALUES_16_BIT
+)
+_TEST_VALUES_32_BIT.sort()
+TEST_VALUES_32_BIT = [(value, 32) for value in _TEST_VALUES_32_BIT]
+TEST_VALUES_32_BIT_COUNTS = list(
+    set(
+        [
+            (((2**bit_count) - 1) & value, bit_count)
+            for bit_count in range(8, 32 + 8, 8)
+            for value in _TEST_VALUES_32_BIT
+        ]
+    )
+)
+TEST_VALUES_32_BIT_COUNTS.sort()
+_TEST_VALUES_32_BIT_UINT_BE = [
+    (value, bit_count)
+    for value, bit_count in TEST_VALUES_32_BIT_COUNTS
+    if get_uint_value(value=value, bit_count=bit_count, endian="big") == value
+]
+_TEST_VALUES_32_BIT_UINT_LE = [
+    (value, bit_count)
+    for value, bit_count in TEST_VALUES_32_BIT_COUNTS
+    if get_uint_value(value=value, bit_count=bit_count, endian="little") == value
+]
+_TEST_VALUES_32_BIT_UINT_BE.sort()
+_TEST_VALUES_32_BIT_UINT_LE.sort()
+
+_TEST_VALUES_64_BIT = (
+    list(
+        set(
+            list([(2**i) for i in range(0, 64, 16) if (2**i) < 0x10000000000000000])
+            + list([(2**i) - 1 for i in range(0, 64 + 16, 16)])
+        )
+    )
+    + _TEST_VALUES_32_BIT
+)
+_TEST_VALUES_64_BIT.sort()
+TEST_VALUES_64_BIT = [(value, 64) for value in _TEST_VALUES_64_BIT]
+TEST_VALUES_64_BIT_COUNTS = list(
+    set(
+        [
+            (((2**bit_count) - 1) & value, bit_count)
+            for bit_count in range(16, 64 + 16, 16)
+            for value in _TEST_VALUES_64_BIT
+        ]
+    )
+)
+TEST_VALUES_64_BIT_COUNTS.sort()
+_TEST_VALUES_64_BIT_UINT_BE = [
+    (value, bit_count)
+    for value, bit_count in TEST_VALUES_64_BIT_COUNTS
+    if get_uint_value(value=value, bit_count=bit_count, endian="big") == value
+]
+_TEST_VALUES_64_BIT_UINT_LE = [
+    (value, bit_count)
+    for value, bit_count in TEST_VALUES_64_BIT_COUNTS
+    if get_uint_value(value=value, bit_count=bit_count, endian="little") == value
+]
+_TEST_VALUES_64_BIT_UINT_BE.sort()
+_TEST_VALUES_64_BIT_UINT_LE.sort()
+
+TEST_VALUES_N_BIT_COUNTS_BE = list(
+    set(
+        _TEST_VALUES_08_BIT_UINT_BE
+        + _TEST_VALUES_16_BIT_UINT_BE
+        + _TEST_VALUES_32_BIT_UINT_BE
+        + _TEST_VALUES_64_BIT_UINT_BE
+    )
+)
+TEST_VALUES_N_BIT_COUNTS_LE = list(
+    set(
+        _TEST_VALUES_08_BIT_UINT_LE
+        + _TEST_VALUES_16_BIT_UINT_LE
+        + _TEST_VALUES_32_BIT_UINT_LE
+        + _TEST_VALUES_64_BIT_UINT_LE
+    )
+)
 TEST_VALUES_N_BIT_UINT_BE_VARIABLE = [
     pytest.param(
         value,
